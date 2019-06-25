c276ba29b02b17ea3175ba0ffb1f54b1326db7d9
Almaev Illia <chytvs777@mail.ru>
2019-06-26 00:55:35 +0300

    test

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Template from '../../components/layout/Template'
 
-const Home = () => <Template>Constructor</Template>
+const Home = () => <Template>Constructor1</Template>
 
 export default Home
